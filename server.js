c6d1796302a533c72c831cadfd7e72ed944d2307
Mario Roque <A01282301@itesm.mx>
2020-05-02 23:50:46 -0500

    Added database

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const morgan = require('morgan');
 const uuid = require('uuid');
-
+const {Bookmarks} = require("./bookmarksModel.js");
+const mongoose = require('mongoose');
 
 const APIKEY = "2abbf7c3-245b-404f-9473-ade729ed4653";
 
-app.use( morgan( 'dev' ));
+app.use(morgan( 'dev' ));
 
 function VerifyAPIKEY(req, res, next){
     let BearerKey = req.headers.authorization;
@@ -21,60 +22,17 @@ function VerifyAPIKEY(req, res, next){
     }
 
     next();
-        
 }
 
 app.use( VerifyAPIKEY ); 
 
-
-
-let bookmarks = [ 
-    {
-        id: uuid.v4(),
-        title: "Reddit",
-        description: "A social network",
-        url: "http://www.reddit.com",
-        rating: 5
-    },
-    {
-        id: uuid.v4(),
-        title: "Facebook",
-        description: "A social network",
-        url: "http://www.facebook.com",
-        rating: 5
-    },
-    {
-        id: uuid.v4(),
-        title: "Github",
-        description: "A code repository",
-        url: "http://www.github.com",
-        rating: 5
-    },
-    {
-        id: uuid.v4(),
-        title: "Speed Test",
-        description: "A webpage to do network speed test",
-        url: "http://www.speedtest.net",
-        rating: 5
-    },
-    {
-        id: uuid.v4(),
-        title: "What is my ip",
-        description: "Shows the ip of the user",
-        url: "https://www.whatismyip.com/",
-        rating: 5
-    },
-    {
-        id: uuid.v4(),
-        title: "Wikipedia",
-        description: "A knowleadge webpage",
-        url: "https://www.wikipedia.org",
-        rating: 5
-    }
-]
-
 app.get( '/bookmarks', (req,res)=>{
-    return res.status(200).json(bookmarks);
+    Bookmarks.getBookmarks()
+    .then( result => {return res.status(200).json( result )
+        })
+    .catch( err => {res.statusMessage = "Database error";
+    return res.status(500).end()
+});
 });
 
 app.get('/bookmark', (req,res)=>{
@@ -83,18 +41,14 @@ app.get('/bookmark', (req,res)=>{
         res.statusMessage = "A title was not received";
         res.status(406).end();
     }else{
-
-    let found = bookmarks.filter(found => {
-        found.title == title;
-    });
-
-    if(found.length == []){
-        res.statusMessage = "Unvalid Title";
-        res.status(404).end();
-        
-    }else{
-        res.status(200).json(found);
-    }
+    Bookmarks.getBookmarkTitle(title).then(result => {
+        if(!result[0]){
+            res.statusMessage = "Title not found";
+            return res.status(404).end();
+        }
+        return res.status(200).json(result);
+    }).catch( err => {res.statusMessage = "Database error"
+    return res.status(500).end();});
 }
 return res;
 });
@@ -112,10 +66,15 @@ app.post('/bookmarks', jsonParser, (req,res)=>{
     }
 
     let addBookmark = { id: uuid.v4(), title, description, url, rating};
-    bookmarks.push(addBookmark);
-    return res.status(201).json(addBookmark);
+    Bookmarks.addBookmark(addBookmark)
+    .then(result => 
+        {return res.status(201).json(result);
+        })
+    .catch(err => 
+        {res.statusMessage = "Database error";
+            return res.status(500).end();
+        });
 });
-
 
 app.delete( '/bookmark/:id', (req, res)=>{
     let id = req.params.id;
@@ -124,20 +83,13 @@ app.delete( '/bookmark/:id', (req, res)=>{
         return res.status(406).end();
     }
 
-    let idx = bookmarks.findIndex((element)=>{
-       if( element.id == id)
-           return true;
-    });
-    
-    if(idx == -1){
-       res.statusMessage = "ID not found";
-       return res.status(404).end();
-    }
-
-    bookmarks.splice(idx,1);
-    return res.status(200).end();
+    Bookmarks.deleteBookmarkId(id)
+    .then( result => {return res.status(200).json( result );})
+    .catch( err => {res.statusMessage = "Database error"
+    return res.status(500).end()
 });
 
+});
 
 app.patch( '/bookmark/:id', jsonParser, (req, res)=>{
     let id = req.body.id;
@@ -151,35 +103,51 @@ app.patch( '/bookmark/:id', jsonParser, (req, res)=>{
         return res.status(406).end();
     }
 
-    if(id != req.params.id){
-        res.statusMessage = "Id's are not the same";
-        return res.status(409).end();
-    }
+    let addBookmark = {title,description,url,rating};
 
-    let bookmark = bookmarks.find((element)=>{
-        return element.id == id;
-    });
-
-    if(!bookmark){
-        res.statusMessage = "The bookmark was not found";
-        return res.status(404).end();
-    }
-
-    if(title){
-        bookmark.title = title;
-    }
-    if(description){
-        bookmark.description = description;
-    }
-    if(url){
-        bookmark.url = url;
-    }
-    if(rating){
-        bookmark.rating = rating;
-    }
-    return res.status(202).json(bookmark);
+    Bookmarks.getBookmarkId(id).then(selectedBookmark => {
+        if(selectedBookmark.errmsg){
+            res.statusMessage = "Id not found ";
+            return res.status(404).end();
+        }
+        // Fills the data that is not received with the query
+        if(!title)
+            title = selectedBookmark[0].title
+        if(!description)
+            description = selectedBookmark[0].description
+        if(!url)
+            url = selectedBookmark[0].url
+        if(!rating)
+            rating = selectedBookmark[0].rating
+        //Updates the values
+        Bookmarks.patchBookmark(id,title,description,url,rating).then(
+            patched => {
+            if(patched.errmsg){
+                res.statusMessage = "The data was not patched"
+                return res.status(403).end() //When not patched
+            } return res.status(202).json(patched) //When patched
+        }).catch( err => {res.statusMessage = "Error when patching" //When error due to id
+        return res.status(500).end()})
+    }).catch( err => {res.statusMessage = "Id not found" //When error due to id
+    return res.status(404).end()})
 });
 
 app.listen(8080, ()=>{
     console.log("Server running on http port");
+
+new Promise( (resolve, reject)=>{
+        mongoose.connect( "mongodb://localhost/bookmarksdb", 
+        {useNewUrlParser : true, useUnifiedTopology: true}, (err)=>{
+            if(err)
+                reject(err);
+            else{
+                console.log("Database connected")
+                return resolve();
+            }
+        });
+    }).catch( err => {
+        console.log("Could not connect to the database")
+        mongoose.disconnect();
+        console.log(err);
+    })
 });
